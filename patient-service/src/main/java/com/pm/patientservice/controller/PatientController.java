0cb5324e4b74efd3aac8a4bd96399e732321c519
karnakarkonda <karnamudiraj91@gmail.com>
2026-09-05 17:18:23 +0530

    ff

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -29,9 +29,6 @@ public class PatientController {
     @GetMapping
     @Operation(summary = "Get all patients", description = "Retrieve a list of all patients")
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
-        System.out.println("Get all patients");
-        System.out.println("Get all patients123");
-        System.out.println("Get all patients5678");
         return ResponseEntity.ok().body(patientSerivce.getAllPatients());
     }
 
