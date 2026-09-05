ca62bd08adbb71f54729d2213db0d04478040e09
karnakarkonda <karnamudiraj91@gmail.com>
2026-09-05 15:20:37 +0530

    added some test

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -29,6 +29,7 @@ public class PatientController {
     @GetMapping
     @Operation(summary = "Get all patients", description = "Retrieve a list of all patients")
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
+        System.out.println("Get all patients");
         return ResponseEntity.ok().body(patientSerivce.getAllPatients());
     }
 
